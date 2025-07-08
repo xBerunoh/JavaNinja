02b0630a5d6a93f0397542ea8963cf4861fd255d
Bruno Eduardo de Carvalho Lucena <57199262+xBerunoh@users.noreply.github.com>
2025-07-08 00:04:43 -0400

    Comentários sobre dados não primitivos. o Que é um método ? São caracteriscas que pode ser dado a uma váriavel. Qual a diferença de Dados Não Primitivos, para Dados Primitivos ? Dados Primitivos, são tipos de dados que não recebem métodos de maneira padrão ! Dados Não Primitivos , podem ser chamados metódos, para fazer alterações sem mudar seu escopo.

--- src/TiposDeDados/DadosNaoPrimitivos.java
+++ src/TiposDeDados/DadosNaoPrimitivos.java
@@ -1,5 +1,9 @@
 package TiposDeDados;
 
 public class DadosNaoPrimitivos {
-
+/*
+* Dados Não Primitivos: String, Array, Class, enum
+* Objetivo: Criar um ninja, e atribuir metodos a ele,
+* Metodos: São caracteriscas das nossas váriaveis,
+* */
 }
