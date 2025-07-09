ca8962afbf5eaac529d12a16507a34be22511008
Bruno Eduardo de Carvalho Lucena <57199262+xBerunoh@users.noreply.github.com>
2025-07-09 19:15:13 -0400

    Estrutura do nosso ninja criado .

--- src/Condicoes/IfAndElse.java
+++ src/Condicoes/IfAndElse.java
@@ -1,4 +1,17 @@
 package Condicoes;
 
 public class IfAndElse {
+    public static void main(String[] args) {
+      /*
+      *IF and Else - CONDIÇÕES
+      * Objetivo: Passar o ninja de nivel de acordo com o número de missões !
+      *  */
+        String nome = "Sasuke Uchiha";
+        int idade = 16;
+        boolean hokage = false;
+        int numberOfQuest = 13;
+
+
+
+    }
 }
