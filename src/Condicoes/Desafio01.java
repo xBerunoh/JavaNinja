75947e30f86b0174d04505cdd225726d04f7c7f2
Bruno Eduardo de Carvalho Lucena <57199262+xBerunoh@users.noreply.github.com>
2025-07-10 11:27:08 -0400

    Criação de 3 ninjas .

---
+++ src/Condicoes/Desafio01.java
@@ -0,0 +1,37 @@
+package Condicoes;
+
+public class Desafio01 {
+    /*
+     * Descrição do Desafio: Crie um programa que representa três ninjas da vila da folha (Konoha)
+     * de "Naruto" e suas respectivas missões.
+     *
+     * Cada ninja tem um *NOME*, uma *IDADE* e uma *MISSÃO* atribuida a ele, com o *nome da missão,
+     * nível de dificuldade e status da conclusão*
+     *
+     * Use Condicionais para verificar se o ninja pode concluir a missão baseado na sua idade:
+     * -> Altere o status da missão , para concluida ou não concluida de acordo com essa lógica e imprima o resultado.
+     * -> Ninjas menores de 15 anos só podem concluir missões de Nível C , ou D. Ninjas com 15 anos ou mais podem concluir missões de qualquer nivel
+     *
+     * Altere o status da missão para concluida
+     *
+     * */
+    String nameNinja1 = "Rock Lee";
+    int idadeNinja1 = 18;
+    String missaoNinja1 = "Ajudar a velinha a pintar sua casa";
+    char nivelQuestNinja1 = 'D';
+    boolean statusConcluido1 = true;
+
+    String nameNinja2 = "Neji";
+    int idadeNinja2 = 16;
+    String missaoNinja2 = "Recuperar uma equipe que foi em uma missão , para coletar informações";
+    char nivelQuestNinja2 = 'A';
+    boolean statusConcluido2 = true;
+
+    String nameNinja3 = "Temari";
+    int idadeNinja3 = 18;
+    String missaoNinja3 = "Coletar informações de um grupo da aldeia da chuva";
+    char nivelQuestNinja3 = 'S';
+    boolean statusConcluido3 = false;
+
+}
+
