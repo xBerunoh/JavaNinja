62c74874401687342fb26601098126fe14373449
Bruno Eduardo de Carvalho Lucena <57199262+xBerunoh@users.noreply.github.com>
2025-07-10 15:31:51 -0400

    Desafio 1 Java10x , utilização do IF (condicao) {resultado} e a utilização de operadores lógicos.

--- src/Condicoes/Desafio01.java
+++ src/Condicoes/Desafio01.java
@@ -15,23 +15,59 @@ public class Desafio01 {
      * Altere o status da missão para concluida
      *
      * */
-    String nameNinja1 = "Rock Lee";
-    int idadeNinja1 = 18;
-    String missaoNinja1 = "Ajudar a velinha a pintar sua casa";
-    char nivelQuestNinja1 = 'D';
-    boolean statusConcluido1 = true;
+    public static void main(String[] args) {
 
-    String nameNinja2 = "Neji";
-    int idadeNinja2 = 16;
-    String missaoNinja2 = "Recuperar uma equipe que foi em uma missão , para coletar informações";
-    char nivelQuestNinja2 = 'A';
-    boolean statusConcluido2 = true;
 
-    String nameNinja3 = "Temari";
-    int idadeNinja3 = 18;
-    String missaoNinja3 = "Coletar informações de um grupo da aldeia da chuva";
-    char nivelQuestNinja3 = 'S';
-    boolean statusConcluido3 = false;
+        String nameNinja1 = "Naruto";
+        int idadeNinja1 = 17;
+        String missaoNinja1 = "Ajudar a velinha a pintar sua casa";
+        char nivelQuestNinja1 = 'A';
+
+
+        String nameNinja2 = "Sasuke";
+        int idadeNinja2 = 16;
+        String missaoNinja2 = "Recuperar uma equipe que foi em uma missão , para coletar informações";
+        char nivelQuestNinja2 = 'B';
+
+
+        String nameNinja3 = "Shikamaru";
+        int idadeNinja3 = 7;
+        String missaoNinja3 = "Coletar informações de um grupo da aldeia da chuva";
+        char nivelQuestNinja3 = 'A';
+
+        if(idadeNinja1 >= 15){
+            System.out.println("Ninja  "+nameNinja1);
+            if(nivelQuestNinja1 == 'A' || nivelQuestNinja1 == 'B' || nivelQuestNinja1 == 'C'){
+                System.out.println(" CONCLUIDO");
+            }else{
+                System.out.println(" Não concluido");
+            }
+        }else{
+            System.out.println(nameNinja2+" Não concluido pela idade");
+        }
+        if(idadeNinja2 >= 15){
+            System.out.println("Ninja  "+nameNinja2);
+            if(nivelQuestNinja2 == 'A' || nivelQuestNinja2 == 'B' || nivelQuestNinja2 == 'C'){
+                System.out.println(" CONCLUIDO");
+            }else{
+                System.out.println(" Não concluido");
+            }
+        }else{
+            System.out.println(nameNinja2+" Não concluido pela idade");
+        }
+        if(idadeNinja3 >= 15){
+            System.out.println("Ninja  "+nameNinja3);
+            if(nivelQuestNinja3 == 'A' || nivelQuestNinja2 == 'B' || nivelQuestNinja3 == 'C'){
+                System.out.println(" CONCLUIDO");
+            }else{
+                System.out.println(" Não concluido");
+            }
+        }else{
+            System.out.println(nameNinja3+ "\n Não concluido pela idade");
+        }
+
+
+    }
 
 }
 
