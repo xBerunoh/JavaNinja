fce2f577e910d48b8fa91836d5fd695914af4474
Bruno Eduardo de Carvalho Lucena <57199262+xBerunoh@users.noreply.github.com>
2025-07-14 10:45:34 -0400

    Desafio 02 - Switch Case, Laços , condcionais , scanner .

---
+++ src/Condicoes/Desafio02.java
@@ -0,0 +1,48 @@
+package Condicoes;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Desafio02 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int menu = 0;
+        int NUMERO_MAX = 5;
+        int qtaNinjas = 0;
+        String[] cadastrarNinja = new String[NUMERO_MAX];
+        while(menu != 3){
+        System.out.println("1 - Cadastrar Ninja");
+        System.out.println("2 - Listar Ninjas");
+        System.out.println("3 - Sair");
+        System.out.println("Escolha alguma opção");
+        menu   = sc.nextInt(); // Usuário entrar com a opção do menu
+        sc.nextLine();
+        switch (menu){
+            case 1:
+                if(qtaNinjas < NUMERO_MAX ){
+                System.out.print("Cadastrar o ninja: ");
+                cadastrarNinja[qtaNinjas] = sc.nextLine();
+                qtaNinjas++;
+                } else {
+                    System.out.println("Valor maximo de ninjas, impossivel de cadastrar ! ");
+                }
+                break;
+            case 2:
+                if(qtaNinjas == 0){
+                    System.out.println("Nenhum ninja foi encontrado !");
+                }else{
+                for (int i = 0; i < qtaNinjas; i++) {
+                    System.out.println(i+" - "+cadastrarNinja[i]);
+                }
+                }
+            case 3:
+                System.out.println("Estamos saindo do programa !");
+                break;
+            default:
+                System.out.println("Opção invalida!");
+                break;
+
+
+        }
+    }
+}}
--- src/Condicoes/lacosDeRepeticao.java
+++ src/Condicoes/lacosDeRepeticao.java
@@ -2,15 +2,15 @@ package Condicoes;
 
 public class lacosDeRepeticao {
     public static void main(String[] args) {
-/*
-* Laços de Repetição: Vão repetir infinitamente ou ate você atingir o parametro definido !
-* WHILE = FOR
-*
-*
-* */
-        int numeroMaximoDeClones = 40;
-        for (int numeroDeClones = 1; numeroDeClones <= numeroMaximoDeClones; numeroDeClones++) {
-            System.out.println(numeroDeClones);
-        }
-    }
+        /*
+         * Laços de Repetição: Vão repetir infinitamente ou ate você atingir o parametro definido !
+         * WHILE = FOR
+         *
+         *
+         * */
+
+        for (double i = 10; i >= 1; i--) {
+
+            System.out.println(i);
+        }}
 }
