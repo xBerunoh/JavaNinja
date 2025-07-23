362eba30b22d807330def93eeb1fe96391882425
bigodinho_notebook <brunowrok61@gmail.com>
2025-07-23 00:18:50 -0400

    Tornando a minha classe ABSTRACT e criando métodos abstract , para se tornar nossa super-classe.

--- src/classesAbstratas/Hokages.java
+++ src/classesAbstratas/Hokages.java
@@ -1,20 +1,21 @@
 package classesAbstratas;
 
-public class Hokages {
+public abstract class Hokages {
     // ATRIBUTOS
     String nome;
     int idade;
     boolean vivoOunao;
 
-// Você consegue criar automaticamente os construtores com o GENERETE : (CTRL + N) SHORT CUTS
+
+    public abstract void sabedoriaHokage(); // método abstrato
     public Hokages(String nome, int idade, boolean vivoOunao) {
-        // `all - args`
+
         this.nome = nome;
         this.idade = idade;
         this.vivoOunao = vivoOunao;
-        // This faz referencia ao seu atributo, o seu atributo recebe como valor o seu PARAMETRO ! criando um construtor
+
     }
-        // Constutor Vazio , sem argumentos !
+
     public Hokages() {
         // `No - args`
     }
