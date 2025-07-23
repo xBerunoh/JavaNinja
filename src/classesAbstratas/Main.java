8dd23e03e1fdb7c030995ba6df21583c6b7e5683
bigodinho_notebook <brunowrok61@gmail.com>
2025-07-23 00:18:44 -0400

    Tornando a minha classe ABSTRACT e criando métodos abstract , para se tornar nossa super-classe.

--- src/classesAbstratas/Main.java
+++ src/classesAbstratas/Main.java
@@ -2,7 +2,7 @@ package classesAbstratas;
 
 public class Main {
     public static void main(String[] args) {
-        Hokages hashirama = new Hokages("hashirama",40,false);
+        Senju hashirama = new Senju("hashirama",40,false);
         Hokages tobirama = new Hokages("Tobirama",49,false);
         Hokages minato = new Hokages("Minato",40,false);
 
