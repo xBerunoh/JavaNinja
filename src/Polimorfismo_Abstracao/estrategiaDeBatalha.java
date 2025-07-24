c872aac3239f55338398d0fbff540bb72a457270
bigodinhonot <brunoeduardo92093676@gmail.com>
2025-07-24 00:16:53 -0400

    Interfaces servem como contratos

--- src/Polimorfismo_Abstracao/estrategiaDeBatalha.java
+++ src/Polimorfismo_Abstracao/estrategiaDeBatalha.java
@@ -1,7 +1,7 @@
 package Polimorfismo_Abstracao;
 
 public interface estrategiaDeBatalha {
-
+    // Interfaces servem como CONTRATOS !!!!
     // Metodo abstrato - Obrigatorio em todas as classes !
     void estrategiaNinja();
     void estrategiaDeCombate();
