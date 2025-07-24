3ab08fb932efd9eede2114ec0330f13d6d2ebcb7
bigodinhonot <brunoeduardo92093676@gmail.com>
2025-07-23 23:50:58 -0400

    Classe MAIN , vamos intacias nossos objetos e chamar nossos metodos.

--- src/Polimorfismo_Abstracao/Main.java
+++ src/Polimorfismo_Abstracao/Main.java
@@ -1,4 +1,9 @@
 package Polimorfismo_Abstracao;
 
 public class Main {
+    public static void main(String[] args) {
+        Uzumaki naruto = new Uzumaki();
+
+        naruto.habilidadeEspecial();
+    }
 }
