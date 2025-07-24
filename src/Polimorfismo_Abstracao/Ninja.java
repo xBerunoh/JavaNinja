7a783c882769bf2e5d32cb3cc82e6d594644dc01
bigodinhonot <brunoeduardo92093676@gmail.com>
2025-07-23 23:50:30 -0400

    Classe Ninja , declarando atributos e métodos.

---
+++ src/Polimorfismo_Abstracao/Ninja.java
@@ -0,0 +1,13 @@
+package Polimorfismo_Abstracao;
+
+public class Ninja {
+    String nome;
+    String aldeia;
+    int idade;
+
+
+    // Método Geral - Todos os ninjas vão ter !
+    public void habilidadeEspecial(){
+        System.out.println("Meu nome é "+nome+ " esse é meu ataque especial!");
+    }
+}
