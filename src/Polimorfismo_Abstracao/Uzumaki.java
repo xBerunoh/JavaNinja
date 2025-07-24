c578653624833f50727643360d02bf3e422440ad
bigodinhonot <brunoeduardo92093676@gmail.com>
2025-07-23 23:50:12 -0400

    Classe Uzumaki que herda da classe Ninja, seus atributos e metodos , sobrescrevendo o método da classe ninja @Override

---
+++ src/Polimorfismo_Abstracao/Uzumaki.java
@@ -0,0 +1,10 @@
+package Polimorfismo_Abstracao;
+
+public class Uzumaki extends Ninja{
+
+    // Sobrescrever o metodo da classe NINJA !
+    @Override
+    public void habilidadeEspecial() {
+        System.out.println("Meu nome é "+nome+" e esse é meu ataque uzumaki !");
+    }
+}
