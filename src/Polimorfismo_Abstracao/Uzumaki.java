bd690326e35bb1ac95ab0372a926253bcf299301
bigodinhonot <brunoeduardo92093676@gmail.com>
2025-07-27 15:39:00 -0400

    Refaranto nosso ataque ninja, adicionando um comentario de quantas missôes naruto fez, adicionado a sobrecarga de construtores

--- src/Polimorfismo_Abstracao/Uzumaki.java
+++ src/Polimorfismo_Abstracao/Uzumaki.java
@@ -2,10 +2,17 @@ package Polimorfismo_Abstracao;
 
 public class Uzumaki extends Ninja{
 
+
+
+    // TODO: Sobrecarga de Construtores na sub classe uzumaki.
+    public Uzumaki(String nome, String aldeia, int idade, int numeroDeMissoesConcluidas, nivelNinja rank) {
+        super(nome, aldeia, idade, numeroDeMissoesConcluidas, rank);
+    }
+
     // Sobrescrever o metodo da classe NINJA !
     @Override
     public void habilidadeEspecial() {
-        System.out.println("Meu nome é "+nome+" e esse é meu ataque uzumaki !");
+        System.out.println("Meu nome é "+nome+" e esse é meu ataque uzumaki, eu já completei:"+numeroDeMissoesConcluidas+" Missôes");
     }
 
     @Override
