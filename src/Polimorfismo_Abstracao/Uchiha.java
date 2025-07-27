92495a44b0d58c1809c15058e4702c6af7e85521
bigodinhonot <brunoeduardo92093676@gmail.com>
2025-07-27 15:21:35 -0400

    Sobrecarga de construtores, utilizando this(); em SUBCLASSES

--- src/Polimorfismo_Abstracao/Uchiha.java
+++ src/Polimorfismo_Abstracao/Uchiha.java
@@ -12,6 +12,10 @@ public class Uchiha  extends Ninja{
         super(nome, aldeia, idade); // Você está referenciando atributos da SUPERCLASSE
     }
 
+    public Uchiha(String nome, String aldeia, int idade, int numeroDeMissoesConcluidas, nivelNinja rank) {
+        super(nome, aldeia, idade, numeroDeMissoesConcluidas, rank);// InteliJ já faz automaticamente em subclasses
+    }
+
     // Sobrescrever o metodo da classe NINJA !
     @Override
     public void habilidadeEspecial() {
