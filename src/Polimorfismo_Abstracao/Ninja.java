8637f9af10add162fd9e8fa04a255d24345a0e1b
bigodinhonot <brunoeduardo92093676@gmail.com>
2025-07-27 17:07:31 -0400

    Chamando meu método inteligenciaDeCombate com parametros , fazendo uma condicional em base no QI do ninja

--- src/Polimorfismo_Abstracao/Ninja.java
+++ src/Polimorfismo_Abstracao/Ninja.java
@@ -50,6 +50,30 @@ public abstract class Ninja implements estrategiaDeBatalha{
     @Override
     public void estrategiaDeCombate() {
         System.out.println("Meu nome: "+nome+ " essa é minha estrategia Ninja");
+
+
+
+
+
+    }
+
+    @Override
+    public void inteligenciaDeCombate() {
+        System.out.println("Meu nome é: "+nome+" Essa é minha INTELIGENCIA de combate !");
+
+    }
+
+    // Sobrecarga de metodos
+
+    @Override
+    public void inteligenciaDeCombate(int qi) {
+        if(qi > 100){
+            System.out.println("Seu QI é:"+qi+" Você é um genio");
+        }else if (qi >= 130){
+            System.out.println("Seu QI é:"+qi+" Você é um Ninja Promissor");
+        }else{
+            System.out.println("Seu QI é:"+qi+" Você precisa treinar mais suas estrategias !");
+        }
     }
 }
 
