304b94fe4d262837199e128894ca4933785cc0c3
bigodinhonot <brunoeduardo92093676@gmail.com>
2025-07-27 15:07:41 -0400

    Utilização de comentarios usando ´TODO´

--- src/Polimorfismo_Abstracao/Ninja.java
+++ src/Polimorfismo_Abstracao/Ninja.java
@@ -5,6 +5,19 @@ public abstract class Ninja implements estrategiaDeBatalha{
     String aldeia;
     int idade;
 
+    // `t o d o ` : Para alerta sobre algo que foi feito ! opção de nevagação !
+    //TODO: Incluir 2 novos atributos : NumeroDeMissaoConcluido , Rank
+
+
+    // Construtor Padrão
+    public Ninja() {
+    }
+    // Construtor "ALl args"
+    public Ninja(String nome, String aldeia, int idade) {
+        this.nome = nome;
+        this.aldeia = aldeia;
+        this.idade = idade;
+    }
 
     // Metodo Geral - Todos os ninjas vão ter !
     public void habilidadeEspecial(){
