70e8ed2aa9bcbceecde2f9a460749e165768685f
bigodinhonot <brunoeduardo92093676@gmail.com>
2025-07-27 15:07:56 -0400

    --

--- src/Polimorfismo_Abstracao/Main.java
+++ src/Polimorfismo_Abstracao/Main.java
@@ -1,5 +1,7 @@
 package Polimorfismo_Abstracao;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -11,11 +13,21 @@ public class Main {
         naruto.habilidadeEspecial();
         naruto.estrategiaNinja();
         naruto.estrategiaDeCombate();
+
         // Nosso objeto Uchiha.
+
         Uchiha sasuke = new Uchiha();
         sasuke.nome = "Sasuke";
         sasuke.estrategiaNinja();
         sasuke.habilidadeEspecial();
         sasuke.estrategiaDeCombate();
+
+        // Objeto uchiha 2
+
+        Uchiha itachi = new Uchiha("Itachi Uchiha","Aldeia da FOlha", 20);
+        itachi.habilidadeEspecial();
+
     }
+
+
 }
--- src/Polimorfismo_Abstracao/Uchiha.java
+++ src/Polimorfismo_Abstracao/Uchiha.java
@@ -2,16 +2,22 @@ package Polimorfismo_Abstracao;
 
 public class Uchiha  extends Ninja{
 
+
+    public Uchiha() {
+        super(); // Você está referenciando atributos da SUPERCLASSE
+
+    }
+
+    public Uchiha(String nome, String aldeia, int idade) {
+        super(nome, aldeia, idade); // Você está referenciando atributos da SUPERCLASSE
+    }
+
     // Sobrescrever o metodo da classe NINJA !
     @Override
     public void habilidadeEspecial() {
         System.out.println("Meu nome é "+nome+" e esse é meu ataque Uchiha !");
     }
 
-    @Override
-    public void estrategiaNinja() {
-        System.out.println("ESsa é minha estrategia de batalha UCHIHA!");
-    }
 
 
 }
