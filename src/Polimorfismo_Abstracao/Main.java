35ce984cc0da6be2569094bbb497a6e4d8dc29fb
bigodinhonot <brunoeduardo92093676@gmail.com>
2025-07-27 20:25:23 -0400

    Chamando nossa referencia do objeto e mostrando o valor dela com a mensagem que coloquei no toString na classe UZUMAKI.

--- src/Polimorfismo_Abstracao/Main.java
+++ src/Polimorfismo_Abstracao/Main.java
@@ -9,26 +9,24 @@ public class Main {
 
         // Nosso objeto uzumaki
         Uzumaki naruto = new Uzumaki("Naruto Uzumaki","Aldeida da Folha",18,23,nivelNinja.GENIN);
-        naruto.habilidadeEspecial();
+        System.out.println(naruto);
 
 
         //  objeto Uchiha 1.
 
         Uchiha sasuke = new Uchiha("Sasuke Uchiha","Aldeia da Folha",19,25,nivelNinja.GENIN);
-        sasuke.habilidadeEspecial();
+
 
 
         // Objeto uchiha 2
 
         Uchiha itachi = new Uchiha("Itachi Uchiha","Aldeia da FOlha", 20,230,nivelNinja.JOUNIN);
-        itachi.habilidadeEspecial();
+
 
         // Objeto Uchiha 3
 
         Uchiha madara = new Uchiha("Marada","Aldeida da Folha",45,400,nivelNinja.KAGE);
-        madara.habilidadeEspecial();
-        madara.estrategiaDeCombate();
-        madara.inteligenciaDeCombate(90);
+
 
     }
 
