fdf30258c1efb8c8072df80d1b1a21ad223eefbe
bigodinhonot <brunoeduardo92093676@gmail.com>
2025-07-27 20:49:53 -0400

    criando atributo saldo e construtor.

--- src/Desafio05/ContaBancaria.java
+++ src/Desafio05/ContaBancaria.java
@@ -1,5 +1,14 @@
 package Desafio05;
 
 public abstract class ContaBancaria implements Conta{
+double saldo;
+
+
+//Construtor `all args`
+public ContaBancaria(double saldo){
+    this.saldo = saldo;
+}
+
+
 
 }
