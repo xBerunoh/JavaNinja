1216a8d35e9ed8df17157d0f41051f3683fa1980
bigodinhonot <brunoeduardo92093676@gmail.com>
2025-07-27 20:47:08 -0400

    Criação da classe Conta Poupança que também extende a classe ContaBancaria, e os métodos.

---
+++ src/Desafio05/ContaPoupanca.java
@@ -0,0 +1,14 @@
+package Desafio05;
+
+public class ContaPoupanca extends ContaBancaria{
+
+    @Override
+    public void consultarSaldo() {
+
+    }
+
+    @Override
+    public void depositar(double valor) {
+
+    }
+}
