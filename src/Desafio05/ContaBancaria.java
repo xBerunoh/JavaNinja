98bbb2d669e63067141975574eae0bbe6b29fbc3
bigodinhonot <brunoeduardo92093676@gmail.com>
2025-07-27 20:57:22 -0400

    Métodos

--- src/Desafio05/ContaBancaria.java
+++ src/Desafio05/ContaBancaria.java
@@ -9,6 +9,13 @@ public ContaBancaria(double saldo){
     this.saldo = saldo;
 }
 
+// Métodos
+    @Override
+    public void consultarSaldo() {
+        System.out.println("Seu saldo atual  é:"+saldo);
+    }
 
-
+    // Metodo abstrato
+    @Override
+    public abstract void depositar(double valor);
 }
