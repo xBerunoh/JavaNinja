a7b21f7289c13190f262bc93730e1ad5ceba5a92
bigodinhonot <brunoeduardo92093676@gmail.com>
2025-07-31 23:24:27 -0400

    Class Final, conceitos e como se declara

---
+++ src/Polimorfismo_Abstracao/Anbu.java
@@ -0,0 +1,11 @@
+package Polimorfismo_Abstracao;
+// `Final class` - essa classe não pode ser estendida !
+// Diferença de uma classe final , para uma abstrata :
+// Class Abstrata , você não pode criar um objeto !
+// Class Final , você não pode estender ela, porém pode criar um objeto !
+final public class Anbu {
+
+    public void anbu(){
+        System.out.println("Eu sou um Anbu !");
+    }
+}
