df9b6bf3bad88d30377f6c66e16db29ee72f33a4
bigodinhonot <brunoeduardo92093676@gmail.com>
2025-08-01 21:53:41 -0400

    Utilizando Setters e Getters

--- src/encapsulamento/Main.java
+++ src/encapsulamento/Main.java
@@ -3,5 +3,12 @@ package encapsulamento;
 public class Main {
     public static void main(String[] args) {
 
+        Uchiha sasuke = new Uchiha("Sasuke","Aldeia da Folha",19,30,1.71);
+        System.out.println(sasuke.getNome()); // Utilizando Get , para mostrar o nome do uchiha sasuke
+        sasuke.setNome("Madara Uchiha"); // utilizando metodo set para alterar o nome do objeto SASUKE.
+        System.out.println(sasuke.getNome());
+
+
+
     }
 }
