239bf5288f833f1fcc5a54ef47084a1b53c38943
Bruno Eduardo de Carvalho Lucena <57199262+xBerunoh@users.noreply.github.com>
2025-10-16 11:36:05 -0400

    Desafio - LinkedList
    Utilizamos funções como toString, get , add , utilizando a estrutura de dados LinkedList.

--- src/Desafio06/Ninjas.java
+++ src/Desafio06/Ninjas.java
@@ -13,10 +13,43 @@ public class Ninjas {
         this.nome = nome;
     }
 
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getVila() {
+        return vila;
+    }
+
+    public void setVila(String vila) {
+        this.vila = vila;
+    }
+
     public Ninjas() {
     }
 
     public void adicionarNinja(){
 
+    }
+
+    @Override
+    public String toString() {
+        return "Ninjas{" +
+                "nome='" + nome + '\'' +
+                ", idade=" + idade +
+                ", vila='" + vila + '\'' +
+                '}';
     }
 }
